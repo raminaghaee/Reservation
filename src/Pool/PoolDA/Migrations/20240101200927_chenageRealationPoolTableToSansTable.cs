48dfe304f2a98f28816e58c8de111f0138942121
ramin aghaee <rn.aghaee@gmail.com>
2024-01-01 23:40:57 +0330

    Modify the pool table relationship

---
+++ src/Pool/PoolDA/Migrations/20240101200927_chenageRealationPoolTableToSansTable.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PoolDA.Migrations
+{
+    /// <inheritdoc />
+    public partial class chenageRealationPoolTableToSansTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pool_Pool_PoolId",
+                schema: "Pool",
+                table: "Pool");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pool_PoolId",
+                schema: "Pool",
+                table: "Pool");
+
+            migrationBuilder.DropColumn(
+                name: "PoolId",
+                schema: "Pool",
+                table: "Pool");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PoolId",
+                schema: "Pool",
+                table: "Pool",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pool_PoolId",
+                schema: "Pool",
+                table: "Pool",
+                column: "PoolId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pool_Pool_PoolId",
+                schema: "Pool",
+                table: "Pool",
+                column: "PoolId",
+                principalSchema: "Pool",
+                principalTable: "Pool",
+                principalColumn: "Id");
+        }
+    }
+}
